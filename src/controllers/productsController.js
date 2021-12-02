62cd8812e59170e6b27d1475ca5a731f222f42fd
JonaPortillo <portillo.jonathan.leonel@gmail.com>
2021-12-02 16:41:13 -0300

    Subida de archivos

---
+++ src/controllers/productsController.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products',{
+			products,
+			toThousand
+		})
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		let productId = +req.params.id
+		let product = products.find(product => product.id === productId)
+
+		res.render('detail',{
+			product,
+			toThousand
+		})
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		res.render('product-create-form')
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		const {name, price, discount, category, description} = req.body
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		// Do the magic
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		// Do the magic
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		// Do the magic
+	}
+};
+
+module.exports = controller;
